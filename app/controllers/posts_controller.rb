511846a61bab2de13363f43ce6282e2016bfaed5
parm530 <parmsahadeo530@gmail.com>
2016-12-09 10:31:50 -0500

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-	def index
-    @posts = Post.all
-	end
+  def index
+    @posts = Post.order(created_at: :desc).page(params[:page])
+  end
 
 	def show
 		@post = Post.find(params[:id])
